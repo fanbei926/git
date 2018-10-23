588954b41306c9163fba76d34bdb6fa3ce8b5321
fanbei926 <imagine.memory@gmail.com>
2018-10-23 21:11:22 +0800

    Update configure-web.sh

--- configure-web.sh
+++ configure-web.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+name=$1
+ip=$2
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-hostnamectl set-hostname Gitlab-server
-sed -i 's#103#104#g' /etc/sysconfig/network-scripts/ifcfg-{eth0,th1}
+hostnamectl set-hostname $name
+sed -i "s#103#$2#g" /etc/sysconfig/network-scripts/ifcfg-{eth0,th1}
 systemctl restart network

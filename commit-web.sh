ad075e71c855ed8b95dec4b53aba59a153e90ad6
fanbei926 <imagine.memory@gmail.com>
2018-10-23 17:40:44 +0800

    Update commit-web.sh

--- commit-web.sh
+++ commit-web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 project_name='html-test'
 filename=`date +%F`_`echo $RANDOM | md5sum | cut -c 1-5`
-host=10.0.0.16
+host=10.0.0.106
 
 yum install -y sshpass
 #ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
@@ -11,4 +11,4 @@ cd /var/lib/jenkins/workspace/${project_name}/
 tar zcf /opt/${filename}.tar.gz ./*
 ssh root@${host} "mkdir -p /usr/share/nginx/${filename}"
 scp /opt/${filename}.tar.gz root@${host}:/usr/share/nginx/${filename}
-ssh root@${host} "cd /usr/share/nginx/${filename} && tar zxf x.tar.gz && rm -fr x.tar.gz && ln -sf /usr/share/nginx/${filename}/ /usr/share/nginx/html"
+ssh root@${host} "cd /usr/share/nginx/${filename} && tar zxf ${filename}.tar.gz && rm -fr ${filename}.tar.gz && ln -sf /usr/share/nginx/${filename}/ /usr/share/nginx/html"

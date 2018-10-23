8659ebed93a1fa2df40b4b421b606fb54a681598
fanbei926 <imagine.memory@gmail.com>
2018-10-23 21:30:28 +0800

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tar zxf /tmp/jdk-8u181-linux-x64.tar.gz -C /tmp/jdk1.8.0_181
+tar zxf /tmp/jdk-8u181-linux-x64.tar.gz -C /tmp/
 mkdir /application
 mv /tmp/jdk1.8.0_181/ /application/
 ln -sf /application/jdk1.8.0_181/ /application/jdk

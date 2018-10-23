3884d9af812bc3d36c188d2c93ab2a4a38dd74cd
fanbei926 <imagine.memory@gmail.com>
2018-10-23 17:35:37 +0800

    Update commit-web.sh

--- commit-web.sh
+++ commit-web.sh
@@ -9,6 +9,6 @@ sshpass -p123456 ssh-copy-id -i /root/.ssh/id_rsa.pub root@${host} "-o StrictHos
 
 cd /var/lib/jenkins/workspace/${project_name}/
 tar zcf /opt/${filename}.tar.gz ./*
-ssh root@10.0.0.106 "mkdir -p /usr/share/nginx/${filename}"
+ssh root@${host} "mkdir -p /usr/share/nginx/${filename}"
 scp /opt/${filename}.tar.gz root@${host}:/usr/share/nginx/${filename}
 ssh root@${host} "cd /usr/share/nginx/${filename} && tar zxf x.tar.gz && rm -fr x.tar.gz && ln -sf /usr/share/nginx/${filename}/ /usr/share/nginx/html"
